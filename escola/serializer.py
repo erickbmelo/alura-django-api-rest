ea69997bd95f432ddec8fb5cdd0efd85ef664741
erick.melo <erick.melo@sorocaba.sp.gov.br>
2023-02-10 17:41:41 -0300

    Criando ViewSets e configurando as URLs

--- escola/serializer.py
+++ escola/serializer.py
@@ -2,7 +2,7 @@ from rest_framework import serializers
 from escola.models import Aluno, Curso
 
 
-class AlunosSerializer(serializers.ModelSerializer):
+class AlunoSerializer(serializers.ModelSerializer):
     class Meta:
         model = Aluno
         fields = ['id', 'nome', 'rg', 'cpf', 'data_nascimento']
